7bbe3a944937dacd4a447de44ba44e5375addee3
AyushiPohekar <ayushi2021.29@gmail.com>
2022-10-28 23:19:39 +0530

    first

---
+++ src/components/PasswordReset.js
@@ -0,0 +1,163 @@
+// import React, { useState } from "react";
+// import { ToastContainer, toast } from "react-toastify";
+
+// const PasswordReset = () => {
+//   const [email, setEmail] = useState("");
+
+//   const [message, setMessage] = useState("");
+
+//   const setVal = (e) => {
+//     setEmail(e.target.value);
+//   };
+
+//   const sendlink = async (e) => {
+//     e.preventDefault();
+
+//     const res = await fetch("/sendpasswordlink", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ email }),
+//     });
+
+//     const data = await res.json();
+
+//     if (data.status == 201) {
+//       setEmail("");
+//       setMessage(true);
+//     }else{
+//         toast.error("Invalid User")
+//     }
+//   };
+//   return (
+//     <>
+//       <section>
+//         <div className="form_data">
+//           <div className="form_heading">
+//             <h1>Enter Your Email</h1>
+//           </div>
+//           {message ? (
+//             <p style={{ color: "green", fontWeight: "bold" }}>
+//               password reset link send Successfully in your Email
+//             </p>
+//           ) : (
+//             ""
+//           )}
+//           <form>
+//             <div className="form_input">
+//               <label htmlFor="email">Email</label>
+//               <input
+//                 type="email"
+//                 name="email"
+//                 id="email"
+//                 placeholder="Enter your email address"
+//                 onChange={setVal}
+//                 value={email}
+//               ></input>
+//             </div>
+
+//             <button className="btn" onClick={sendlink}>
+//               Send
+//             </button>
+//           </form>
+//           <ToastContainer />
+//         </div>
+//       </section>
+//     </>
+//   );
+// };
+
+// export default PasswordReset;
+
+import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import { NavLink } from "react-router-dom";
+import { API } from "./global";
+
+
+const PasswordReset = () => {
+  const [email, setEmail] = useState("");
+
+  const [message, setMessage] = useState("");
+
+  const setVal = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const sendLink = async (e) => {
+    e.preventDefault();
+
+    if (email === "") {
+      toast.error("email is required!", {
+        position: "top-center",
+      });
+    } else if (!email.includes("@")) {
+      toast.warning("includes @ in your email!", {
+        position: "top-center",
+      });
+    } else {
+      const res = await fetch(`${process.env.REACT_APP_BASE_URL}/sendpasswordlink`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      const data = await res.json();
+
+      if (data.status == 201) {
+        setEmail("");
+        setMessage(true);
+      } else {
+        toast.error("Invalid User", {
+          position: "top-center",
+        });
+      }
+    }
+  };
+
+  return (
+    <>
+      <section>
+        <div className="form_data">
+          <div className="form_heading">
+            <h1>Enter Your Email</h1>
+          </div>
+
+          {message ? (
+            <p style={{ color: "green", fontWeight: "bold" }}>
+              pasword reset link send Successfully in Your Email
+            </p>
+          ) : (
+            ""
+          )}
+          <form>
+            <div className="form_input">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                value={email}
+                onChange={setVal}
+                name="email"
+                id="email"
+                placeholder="Enter Your Email Address"
+              />
+            </div>
+
+            <button className="btn" onClick={sendLink}>
+              Send
+            </button>
+            <p>
+              <NavLink to="/">Home</NavLink>
+            </p>
+          </form>
+          <ToastContainer />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default PasswordReset;
---
+++ src/components/global.js
@@ -0,0 +1,1 @@
+export const API="https://authentication-app.onrender.com"
